6640f1b5dfb66c04e619734ba1ab773d6d9f65be
AnanDaraghmeh <anan.daraghmeh@yahoo.com>
2019-04-24 13:17:37 +0200

    minor fixes

--- src/data.js
+++ src/data.js
@@ -1,72 +1,72 @@
-import weather from "./assets/images/weather.PNG";
-import bmi from "./assets/images/bmi.PNG";
-import calculator from "./assets/images/calculator.PNG";
-import movie from "./assets/images/movie.jpg";
-import lexikon from "./assets/images/lexikon.PNG";
-import krona from "./assets/images/krona.PNG";
-import notes from "./assets/images/notes.PNG";
-import blog from "./assets/images/blog.PNG";
+import weather from './assets/images/weather.PNG';
+import bmi from './assets/images/bmi.PNG';
+import calculator from './assets/images/calculator.PNG';
+import movie from './assets/images/movie.jpg';
+import lexikon from './assets/images/lexikon.PNG';
+import krona from './assets/images/krona.PNG';
+import notes from './assets/images/notes.PNG';
+import blog from './assets/images/blog.PNG';
 
 const projects = [
   {
     img: blog,
-    name: "Blog",
+    name: 'Blog',
     desc:
-      "<strong>Gatsby</strong> blog with <strong>contentful</strong> CMS as a backend.",
-    link: "https://reactforcats.netlify.com",
-    code: "https://github.com/AnanDaraghmeh/blog"
+      '<strong>Gatsby</strong> blog with <strong>contentful CMS</strong> as a backend.',
+    link: 'https://reactforcats.netlify.com',
+    code: 'https://github.com/AnanDaraghmeh/blog'
   },
   {
     img: notes,
-    name: "MY NOTES",
+    name: 'MY NOTES',
     desc:
-      "CRUD app with <strong>react/redux</strong> and <strong>firebase</strong> (firestore and auth).",
-    link: "https://mynotes-d565a.firebaseapp.com",
-    code: "https://github.com/AnanDaraghmeh/MY-NOTES"
+      'CRUD app with <strong>react/redux</strong> and <strong>firebase</strong> (firestore and auth).',
+    link: 'https://mynotes-d565a.firebaseapp.com',
+    code: 'https://github.com/AnanDaraghmeh/MY-NOTES'
   },
   {
     img: weather,
-    name: "Weather cards",
+    name: 'Weather cards',
     desc:
-      "React/redux app, powered by <strong>openweathermap.org API.</strong>",
-    link: "https://weather-cards.netlify.com",
-    code: "https://github.com/AnanDaraghmeh/weather-cards"
+      '<strong>React/redux</strong> app, powered by <strong>openweathermap.org API.</strong>',
+    link: 'https://weather-cards.netlify.com',
+    code: 'https://github.com/AnanDaraghmeh/weather-cards'
   },
   {
     img: krona,
-    name: "Krona",
-    desc: "React Currency Converter",
-    link: "https://krona-currency-converter.netlify.com",
-    code: "https://github.com/AnanDaraghmeh/krona"
+    name: 'Krona',
+    desc: '<strong>React</strong> currency converter.',
+    link: 'https://krona-currency-converter.netlify.com',
+    code: 'https://github.com/AnanDaraghmeh/krona'
   },
   {
     img: movie,
-    name: "PoP MOViES",
-    desc: "A movie web app, powered by <strong>The Movie DB API.</strong>",
-    link: "https://pop-movies.netlify.com",
-    code: "https://github.com/AnanDaraghmeh/movie-app"
+    name: 'PoP MOViES',
+    desc: 'A movie web app, powered by <strong>The Movie DB API.</strong>',
+    link: 'https://pop-movies.netlify.com',
+    code: 'https://github.com/AnanDaraghmeh/movie-app'
   },
   {
     img: lexikon,
-    name: "Lexikon",
+    name: 'Lexikon',
     desc:
-      "Swedish-Arabic dictionary, powered by <strong>Glosbe - the multilingual online dictionary.</strong>",
-    link: "https://lexikon.netlify.com",
-    code: "https://github.com/AnanDaraghmeh/lexikon"
+      'Swedish-Arabic dictionary, powered by <strong>Glosbe - the multilingual online dictionary.</strong>',
+    link: 'https://lexikon.netlify.com',
+    code: 'https://github.com/AnanDaraghmeh/lexikon'
   },
   {
     img: calculator,
-    name: "Calculator",
-    desc: "React Calculator.",
-    link: "https://simple-calculator-app.netlify.com",
-    code: "https://github.com/AnanDaraghmeh/calc-react"
+    name: 'Calculator',
+    desc: '<strong>React</strong> calculator.',
+    link: 'https://simple-calculator-app.netlify.com',
+    code: 'https://github.com/AnanDaraghmeh/calc-react'
   },
   {
     img: bmi,
-    name: "BMI calculator",
-    desc: "React BMI calculator.",
-    link: "https://react-bmi-calculator.netlify.com",
-    code: "https://github.com/AnanDaraghmeh/bmi-react"
+    name: 'BMI Calculator',
+    desc: '<strong>React</strong> BMI calculator.',
+    link: 'https://react-bmi-calculator.netlify.com',
+    code: 'https://github.com/AnanDaraghmeh/bmi-react'
   }
 ];
 
